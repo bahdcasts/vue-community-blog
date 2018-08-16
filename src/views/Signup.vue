05669f8884f1ded25fabd6282cb7869f7a5c1ab2
Kati Frantz <frantzenterprises777@gmail.com>
2018-08-17 00:05:36 +0100

    display validation errors

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -5,13 +5,22 @@
         <div class="card-body">
           <h3 class="text-center my-4">Signup</h3>
           <div class="form-group">
-            <input v-model="name" type="text" placeholder="Name" class="form-control">
+            <input v-bind:class="{ 'is-invalid': errors.name, 'is-valid': !errors.name && this.submitted }" v-model="name" type="text" placeholder="Name" class="form-control">
+            <div class="errors" v-if="errors.name">
+              <small class="text-danger" :key="error" v-for="error in errors.name">{{ error }}</small>
+            </div>
           </div>
           <div class="form-group">
-            <input v-model="email" type="text" placeholder="Email" class="form-control">
+            <input v-bind:class="{ 'is-invalid': errors.email, 'is-valid': !errors.email && this.submitted }" v-model="email" type="text" placeholder="Email" class="form-control">
+            <div class="errors" v-if="errors.email">
+              <small class="text-danger" :key="error" v-for="error in errors.email">{{ error }}</small>
+            </div>
           </div>
           <div class="form-group">
-            <input v-model="password" type="password" placeholder="Password" class="form-control">
+            <input v-bind:class="{ 'is-invalid': errors.password, 'is-valid': !errors.password && this.submitted }" v-model="password" type="password" placeholder="Password" class="form-control">
+            <div class="errors" v-if="errors.password">
+              <small class="text-danger" :key="error" v-for="error in errors.password">{{ error }}</small>
+            </div>
           </div>
           <div class="form-group text-center">
             <button @click="registerUser()" class="btn form-control btn-success">Signup</button>
@@ -30,7 +39,9 @@ export default {
     return {
       name: "",
       email: "",
-      password: ""
+      password: "",
+      errors: {},
+      submitted: false
     };
   },
   methods: {
@@ -40,15 +51,17 @@ export default {
         email: this.email,
         password: this.password
       })
-        .then((response) => {
+        .then(response => {
+          this.submitted = true;
           const { data } = response.data;
-          localStorage.setItem('auth', JSON.stringify(data))
+          localStorage.setItem("auth", JSON.stringify(data));
           this.$root.auth = data;
 
-          this.$router.push('home');
+          this.$router.push("home");
         })
         .catch(({ response }) => {
-          console.log(response);
+          this.submitted = true;
+          this.errors = response.data;
         });
     }
   }
