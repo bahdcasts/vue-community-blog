beff9c9c0750def4fc916c6472537edf61c0da00
Kati Frantz <frantzenterprises777@gmail.com>
2018-08-16 22:44:43 +0100

    add router-link

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,14 +10,20 @@
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
-          <li class="nav-item dropdown">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/login">Login</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/signup">Signup</router-link>
+          </li>
+          <!-- <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Hey Bahdcoder
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
               <a class="dropdown-item" href="#">Logout</a>
             </div>
-          </li>
+          </li> -->
         </ul>
       </div>
     </div>
