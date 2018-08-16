b4304554538b3aed70c6009fae852243aadaa832
Kati Frantz <frantzenterprises777@gmail.com>
2018-08-16 22:50:22 +0100

    add design for auth pages

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <Navbar />
-    <router-view></router-view>
+    <div class="container">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>SIGNUP COMPONENT</h1>
+  <div class="row my-5">
+    <div class="col-md-6 offset-md-3">
+      <div class="card">
+        <div class="card-body">
+          <h3 class="text-center my-4">Signup</h3>
+          <div class="form-group">
+            <input type="text" placeholder="Name" class="form-control">
+          </div>
+          <div class="form-group">
+            <input type="text" placeholder="Email" class="form-control">
+          </div>
+          <div class="form-group">
+            <input type="password" placeholder="Password" class="form-control">
+          </div>
+          <div class="form-group text-center">
+            <button class="btn form-control btn-success">Signup</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
