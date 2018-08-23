e319be3a7a792778143ffcc7c2e402cb689194fc
Kati Frantz <frantzenterprises777@gmail.com>
2018-08-23 06:48:23 +0100

    add pagination links

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="row">
-    <div class="col-md-8 offset-md-2" v-if="articles.data" v-for="article in articles.data" :key="article.id">
-      <Article :article="article" />
+  <div>
+    <div class="d-flex mt-4 justify-content-between">
+      <button @click="getPrevArticles()" :disabled="articles.prev_page_url === null" class="btn btn-warning">Prev Page</button>
+      <button @click="getNextArticles()" :disabled="articles.next_page_url === null" class="btn btn-warning">Next Page</button>
+    </div>
+    <div class="row" v-if="!loading">
+      <div class="col-md-8 offset-md-2" v-for="article in articles.data" :key="article.id">
+        <Article :article="article" />
+      </div>
+    </div>
+    <div class="loader text-center" v-else>
+      <i class="fas fa-3x fa-spin fa-spinner"></i>
     </div>
   </div>
 </template>
@@ -21,15 +30,30 @@ export default {
   },
   data() {
     return {
-      articles: {}
+      articles: {},
+      loading: true
     };
   },
   methods: {
-    getArticles() {
-      Axios.get(`${config.apiUrl}/articles`).then(response => {
+    getArticles(url = `${config.apiUrl}/articles`) {
+      this.loading = true;
+      Axios.get(url).then(response => {
+        this.loading = false;
         this.articles = response.data.data;
       });
+    },
+    getNextArticles() {
+      this.getArticles(this.articles.next_page_url);
+    },
+    getPrevArticles() {
+      this.getArticles(this.articles.prev_page_url);
     }
   }
 };
 </script>
+
+<style>
+  .btn-warning {
+    color: #fff;
+  }
+</style>
