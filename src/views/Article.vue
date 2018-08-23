b9d02b520cc4e43bc4731e0d0b85071abffe23f4
Kati Frantz <frantzenterprises777@gmail.com>
2018-08-23 05:53:44 +0100

    display single article details

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,5 +1,20 @@
 <template>
-  <h1 class="text-center">{{ $route.params.id }}</h1>
+  <div class="container my-5">
+    <div class="row">
+      <div class="col-md-10 offset-md-1">
+        <div class="card" v-if="!loading">
+          <img height="420px" :src="article.imageUrl" alt="" class="card-img-top">
+          <div class="card-body">
+            <h1 class="card-title text-center my-3">{{ article.title }}</h1>
+            <div class="article-content" v-html="article.content"></div>
+          </div>
+        </div>
+        <div class="loader text-center" v-else>
+          <i class="fas fa-5x fa-spin fa-spinner"></i>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,10 +25,17 @@ export default {
   mounted() {
     this.getArticle();
   },
+  data() {
+    return {
+      article: {},
+      loading: true,
+    }
+  },
   methods: {
     getArticle() {
       Axios.get(`${config.apiUrl}/article/${this.$route.params.id}`).then(response => {
-        console.log(response);
+        this.loading = false;
+        this.article = response.data.data;
       });
     }
   }
