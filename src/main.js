703154c0cbf008c1e449867d16aff782993e3532
Kati Frantz <frantzenterprises777@gmail.com>
2018-08-23 06:16:55 +0100

    add disqus comments

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import VueNoty from "vuejs-noty";
+import VueDisqus from 'vue-disqus'
 
 import "vuejs-noty/dist/vuejs-noty.css";
 
 Vue.use(VueNoty);
+Vue.use(VueDisqus);
 
 Vue.config.productionTip = false;
 
--- src/views/Article.vue
+++ src/views/Article.vue
@@ -7,6 +7,10 @@
           <div class="card-body">
             <h1 class="card-title text-center my-3">{{ article.title }}</h1>
             <div class="article-content" v-html="article.content"></div>
+
+            <div class="comments my-4">
+              <vue-disqus shortname="community-blog" :identifier="article.slug" :url="url"></vue-disqus>
+            </div>
           </div>
         </div>
         <div class="loader text-center" v-else>
@@ -29,6 +33,7 @@ export default {
     return {
       article: {},
       loading: true,
+      url: window.location.href
     }
   },
   methods: {
