ec6b9d98dac9b8aa93ce191e15284934b2f91feb
Kati Frantz <frantzenterprises777@gmail.com>
2018-08-24 07:55:08 +0100

    add cloudinary upload

--- src/views/CreateArticle.vue
+++ src/views/CreateArticle.vue
@@ -4,10 +4,15 @@
       <div class="col-md-8 offset-md-2">
         <div class="card my-5">
           <div class="card-body">
+            <picture-input
+              accept="image/jpeg,image/png" 
+              size="5"
+              button-class="btn btn-danger"
+              @change="onChange" />
             <input type="text" placeholder="Title" class="mb-3 form-control">
             <wysiwyg v-model="content" />
             <div class="text-center">
-              <button class="btn-success btn btn-lg mt-3">Create Article</button>
+              <button @click="createArticle()" class="btn-success btn btn-lg mt-3">Create Article</button>
             </div>
           </div>
         </div>
@@ -17,11 +22,32 @@
 </template>
 
 <script>
+import Axios from 'axios';
+import PictureInput from "vue-picture-input";
+
 export default {
+  components: {
+    PictureInput
+  },
   data() {
     return {
-      content: ""
+      content: "",
+      image: null
     };
+  },
+  methods: {
+    onChange(image) {
+      this.image = image;
+    },
+    createArticle() {
+      const form = new FormData();
+      form.append('file', this.image);
+      form.append('upload_preset', 'g5ziunzg');
+      form.append('api_key', '132255634713478');
+
+      Axios.post('https://api.cloudinary.com/v1_1/bahdcoder/image/upload', form)
+        .then(res => console.log(res));
+    }
   }
 };
 </script>
