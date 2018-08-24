0b7b14b827b42faeefbc27c6121edc474ff7ac34
Kati Frantz <frantzenterprises777@gmail.com>
2018-08-24 08:38:55 +0100

    saving articles to api

--- src/views/CreateArticle.vue
+++ src/views/CreateArticle.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <div class="row">
+    <div class="row mb-50">
       <div class="col-md-8 offset-md-2">
         <div class="card my-5">
           <div class="card-body">
@@ -15,10 +15,13 @@
                 {{ category.name }}
               </option>
             </select>
-            <input type="text" placeholder="Title" class="my-3 form-control">
+            <input type="text" placeholder="Title" v-model="title" class="my-3 form-control">
             <wysiwyg v-model="content" />
             <div class="text-center">
-              <button @click="createArticle()" class="btn-success btn btn-lg mt-3">Create Article</button>
+              <button :disabled="loading" @click="createArticle()" class="btn-success btn mt-3">
+                <i class="fas fa-spin fa-spinner" v-if="loading"></i>
+                {{ loading ? '' : 'Create Category' }}
+              </button>
             </div>
           </div>
         </div>
@@ -33,6 +36,13 @@ import config from "@/config";
 import PictureInput from "vue-picture-input";
 
 export default {
+  beforeRouteEnter(to, from, next) {
+    if (!localStorage.getItem("auth")) {
+      return next({ path: "/login" });
+    }
+
+    next();
+  },
   mounted() {
     this.getCategories();
   },
@@ -41,10 +51,12 @@ export default {
   },
   data() {
     return {
+      title: "",
       content: "",
       image: null,
       categories: [],
-      category: ""
+      category: "",
+      loading: false
     };
   },
   methods: {
@@ -52,14 +64,47 @@ export default {
       this.image = image;
     },
     createArticle() {
+      if (!this.title || !this.image || !this.category || !this.content) {
+        this.$noty.error("Please fill in all fields.");
+        return;
+      }
+
+      this.loading = true;
       const form = new FormData();
       form.append("file", this.image);
       form.append("upload_preset", process.env.VUE_APP_CLOUDINARY_PRESET);
       form.append("api_key", process.env.VUE_APP_CLOUDINARY_API_KEY);
 
-      Axios.post(process.env.VUE_APP_CLOUDINARY_URL, form).then(res =>
-        console.log(res)
-      );
+      Axios.post(process.env.VUE_APP_CLOUDINARY_URL, form)
+        .then(res =>
+          Axios.post(
+            `${config.apiUrl}/articles`,
+            {
+              title: this.title,
+              content: this.content,
+              category_id: this.category,
+              imageUrl: res.data.secure_url
+            },
+            {
+              headers: {
+                Authorization: `Bearer ${this.$root.auth.token}`
+              }
+            }
+          )
+            .then(() => {
+              this.loading = false;
+              this.$noty.success("Article created successfully.");
+              this.$router.push("/");
+            })
+            .catch(() => {
+              this.loading = false;
+              this.$noty.error("Oops ! Something went wrong.");
+            })
+        )
+        .catch(() => {
+          this.loading = false;
+          this.$noty.error("Oops ! Something went wrong.");
+        });
     },
     getCategories() {
       const categories = localStorage.getItem("categories");
@@ -76,3 +121,9 @@ export default {
   }
 };
 </script>
+
+<style>
+.mb-50 {
+  margin-bottom: 500px;
+}
+</style>
